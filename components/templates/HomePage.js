88fc68115bc952222ecf184c22b924b4ec03070f
Mohammad Ali <aliparkour83@gmail.com>
2023-11-04 21:21:50 +0330

    Home page Added

---
+++ components/templates/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './HomePage.module.css'
+
+const HomePage = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+};
+
+export default HomePage;

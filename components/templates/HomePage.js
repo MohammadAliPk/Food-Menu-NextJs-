4d2e795997ed32510c7c02ff32494ca3ec757aa0
Mohammad Ali <aliparkour83@gmail.com>
2023-11-04 21:30:12 +0330

    Definition section added to home page

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import styles from './HomePage.module.css'
 import Banner from '../modules/Banner';
+import Attributes from '../modules/Attribute';
+import Definition from '../modules/Definition';
 
 const HomePage = () => {
     return (
         <div className={styles.container}>
             <Banner />
+            <Attributes />
+            <Definition />
         </div>
     );
 };

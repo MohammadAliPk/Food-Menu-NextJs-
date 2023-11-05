3be314db124830b21232c6e66826ed1b07a3d0c8
Mohammad Ali <aliparkour83@gmail.com>
2023-11-05 22:18:26 +0330

    Restrictions part added to home page

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+// styles
 import styles from './HomePage.module.css'
+
+// Modules
 import Banner from '../modules/Banner';
 import Attributes from '../modules/Attribute';
 import Definition from '../modules/Definition';
+import Companies from '../modules/Companies';
+import Instruction from '../modules/Instruction';
+import Guide from '../modules/Guide';
+import Restrictions from '../modules/Restrictions';
 
 const HomePage = () => {
     return (
@@ -11,6 +18,10 @@ const HomePage = () => {
             <Banner />
             <Attributes />
             <Definition />
+            <Companies />
+            <Instruction />
+            <Guide />
+            <Restrictions />
         </div>
     );
 };
